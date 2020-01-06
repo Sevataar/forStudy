079d6b6cfc7297e5875ac2b01a38c42ce5967a8f
Sevataar <sasha.06sas@gmail.com>
2020-01-06 16:27:04 +0300

    1

---
+++ WebApp/public/javascripts/skripts.js
@@ -0,0 +1,67 @@
+"use strict";
+// alert("Не лезь! Оно тебя сжрёт!");
+
+// const myBirthday = "29.06.1999";
+
+// alert(`Привет, ${user}!`);
+// alert(`результат: ${1 + 2}`);
+// результат: 3;
+
+// let nameFieldChecked = true;
+// let ageFieldChecked = false;
+
+// let isGreater = 5 > 2;
+// console.log(isGreater);
+
+// let apple = prompt("Ввести яблоки", "");
+// apple = apple % 1000;
+// if (apple == 1) {
+//   alert(`1 яблоко`);
+// } else if (2 <= apple <= 4) {
+//   alert(`${apple} яблока`);
+// } else if (11 <= apple <= 19) {
+//   alert(`${apple} яблок`);
+// }
+
+// let i = 0;
+// while (0 < 3) {
+//   console.log(i);
+//   i++;
+// }
+
+// let a = 0;
+// do {
+//   console.log(a);
+//   a++;
+// } while (a < 3);
+
+// let num;
+// do {
+//   num = prompt("Введите число юольше 100", 0);
+// } while (num <= 100 && num);
+
+// for (i = 0; i < 10; i++) {
+//   if (i % 2 == 0);
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//   if (i % 2 == 0) continue;
+//   alert(i);
+// }
+
+// outer: for (let i = 0; i < 3; i++) {
+//   for (let j = 0; j < 3; j++) {
+//     let input = prompt(`Значение на координатах (${i}, ${j})`, "");
+//     if (!input) break outer;
+//   }
+// }
+// alert("Готово");
+
+// let n = 10;
+// nextPrime: for (let i = 2; i <= n; i++) {
+//   for (let j = 2; j < i; j++) {
+//     if (i % j == 0) continue nextPrime;
+//   }
+//   alert(i);
+// }
