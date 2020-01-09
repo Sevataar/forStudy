91f5e3fbdefc137cc82e7df9aebaeeaee626880d
Sevataar <sasha.06sas@gmail.com>
2020-01-09 12:43:21 +0300

    1

--- WebApp/public/javascripts/skripts.js
+++ WebApp/public/javascripts/skripts.js
@@ -66,9 +66,9 @@
 //   alert(i);
 // }
 
-for (let i = 0; i <= 35; i++) {
-  if (i % 3 == 0) alert(Fizz);
-  else if (i % 5 == 0) alert(Buzz);
-  else if (i % 3 == 0 || i % 5 == 0) alert(FizzBuzz);
-  else alert(i);
+for (let i = 1; i <= 15; i++) {
+  if (i % 3 == 0) console.log("Fizz");
+  else if (i % 5 == 0) console.log("Buzz");
+  else if (i % 3 == 0 && i % 5 == 0) console.log("FizzBuzz");
+  else console.log(i);
 }
