855785080458ca6debdeed89f80f2a2e84bbf004
Sevataar <sasha.06sas@gmail.com>
2020-01-09 12:09:34 +0300

    1

--- WebApp/public/javascripts/skripts.js
+++ WebApp/public/javascripts/skripts.js
@@ -65,3 +65,10 @@
 //   }
 //   alert(i);
 // }
+
+for (let i = 0; i <= 35; i++) {
+  if (i % 3 == 0) alert(Fizz);
+  else if (i % 5 == 0) alert(Buzz);
+  else if (i % 3 == 0 || i % 5 == 0) alert(FizzBuzz);
+  else alert(i);
+}
