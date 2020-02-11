fda0443e9d9078c81b4debff1c5b736cdbff4cb5
Sevataar <sasha.06sas@gmail.com>
2020-02-11 16:33:09 +0300

    code review

--- WebApp/routes/index.js
+++ WebApp/routes/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__basedir, "/index.html"));
+  res.sendFile(path.join(__basedir, "index.html"));
 });
 
 module.exports = router;
